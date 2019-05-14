3f06783728fbf5ce7d5bbf301f96663dfca8b905
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-14 17:33:12 +0200

    ngmodel added

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: []
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent  {
 
   constructor() { }
 
