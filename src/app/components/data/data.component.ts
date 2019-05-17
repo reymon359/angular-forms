2c467519494ffe5d7274881a06cb70cf5c52a20b
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-17 16:10:26 +0200

    grouping form controls in form group

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -10,11 +10,25 @@ export class DataComponent {
 
   userform: FormGroup;
 
+  user: Object = {
+    fullname: {
+      name: "ramon",
+      surname: "morcillo"
+    },
+    email: "hellocaramelou@gmail.com"
+  };
+
+
   constructor() {
 
+    console.log(this.user); 
+
     this.userform = new FormGroup({
-      'name': new FormControl('', [Validators.required, Validators.minLength(3)]),
-      'surname': new FormControl('', Validators.required),
+      'fullname': new FormGroup({
+        'name': new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'surname': new FormControl('', Validators.required)
+      })
+      ,
       'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
     });
 
