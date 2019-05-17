1391af32aff5aaf64c65062fdc7f6aa7f2a4c071
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-17 15:48:35 +0200

    using validators

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,28 +1,28 @@
-import { Component} from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent  {
+export class DataComponent {
 
-    userform:FormGroup;
+  userform: FormGroup;
 
   constructor() {
 
-    this.userform= new FormGroup({
-      'name': new FormControl('Ramon'),
-      'surname': new FormControl(),
-      'email': new FormControl()
+    this.userform = new FormGroup({
+      'name': new FormControl('', Validators.required),
+      'surname': new FormControl('', Validators.required),
+      'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
     });
 
   }
 
-saveChanges(){
-  console.log(this.userform.value);
-  console.log(this.userform);
-}
+  saveChanges() {
+    console.log(this.userform.value);
+    console.log(this.userform);
+  }
 
 }
